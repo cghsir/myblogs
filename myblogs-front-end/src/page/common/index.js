8fac632ae5fb217990eea14e7a1795e74d0c695d
cghsir <cghsir@gmail.com>
2017-12-25 15:48:21 +0800

    f-002-webpack配置与common

---
+++ myblogs-front-end/src/page/common/index.js
@@ -0,0 +1,7 @@
+/*
+* @Author: cghsir
+* @Date:   2017-12-25 15:06:06
+* @Last Modified by:   cghsir
+* @Last Modified time: 2017-12-25 15:19:05
+*/
+console.log('i am gloable');
---
+++ myblogs-front-end/src/page/index/index.js
@@ -0,0 +1,7 @@
+/*
+* @Author: cghsir
+* @Date:   2017-12-25 15:07:51
+* @Last Modified by:   cghsir
+* @Last Modified time: 2017-12-25 15:33:58
+*/
+alert('index');
