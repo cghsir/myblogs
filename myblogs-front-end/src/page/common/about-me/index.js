7b5f63a536cad06cccdcb98c53cf35001520d6d4
cghsir <cghsir@gmail.com>
2017-12-27 16:12:23 +0800

    f-005-首页边栏about-me

---
+++ myblogs-front-end/src/page/common/about-me/index.js
@@ -0,0 +1,7 @@
+/*
+* @Author: cghsir
+* @Date:   2017-12-27 14:52:00
+* @Last Modified by:   cghsir
+* @Last Modified time: 2017-12-27 14:52:52
+*/
+require('./index.css');
--- myblogs-front-end/src/page/index/index.js
+++ myblogs-front-end/src/page/index/index.js
@@ -2,11 +2,12 @@
 * @Author: cghsir
 * @Date:   2017-12-25 15:07:51
 * @Last Modified by:   cghsir
-* @Last Modified time: 2017-12-27 10:55:16
+* @Last Modified time: 2017-12-27 14:52:40
 */
 require('page/common/header/index.js');
 require('page/common/search-con/index.js');
 require('page/common/home-tips/index.js');
-require('page/common/notice/index.js');
 require('page/common/banner/index.js');
 require('page/common/article-list/index.js');
+require('page/common/notice/index.js');
+require('page/common/about-me/index.js');
